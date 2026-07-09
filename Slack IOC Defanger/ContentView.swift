a435423a8cc524459ddc5c2122c63f70517dbd87
newyork167 <newyork167@gmail.com>
2026-07-09 17:17:03 -0400

    Initial Commit

---
+++ Slack IOC Defanger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Slack IOC Defanger
+//
+//  Created by Cody Dietz on 7/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Slack IOC Defanger/Slack_IOC_DefangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slack_IOC_DefangerApp.swift
+//  Slack IOC Defanger
+//
+//  Created by Cody Dietz on 7/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Slack_IOC_DefangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
